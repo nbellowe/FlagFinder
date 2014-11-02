9185716a403d5bea53685ee0774124dc8a989190
nbellowe <nbellowe@gmail.com>
2014-11-01 21:53:15 -0600

    Update __init__.py

--- scripts/ff/__init__.py
+++ scripts/ff/__init__.py
@@ -1,14 +1,14 @@
 # universal functions (in pkg)
+#http://beyondgrep.com/ only searches source code, also better. e.g. ack --perl REGEX_PATTERN only searches prl
 
 import subprocess, sys, comment_linecache, re
 
-from collections import namedtuple, Counter
-#Alex, challenge for you in particular. For your first project, make any of these be able to be passed either a comment_line array, (multicomment_line comments), or a single comment_line.
-
+from collections import namedtuple
+#Alex, challenge for you in particular. For your first project, make any of these be able to be passed either a comment_line array, (multicomment_line comments), or a single comment_line. Also, you'll be sad to know, I already commented this and deleted them! Figure a couple out, and text me if you need help.
 
 TODO_FLAGS = 'TODO' #note, parse yaml, make someone else do the rest of this shit, because to much design work. Make array-capable
 
-Match = namedtuple('Match', ['file', 'comment_line_number', 'comment_line'])
+Flag_Line = namedtuple('Flag_Line', ['file', 'comment_line_number', 'comment_line']) #https://docs.python.org/2/library/collections.html
 
 def check_comment_line(comment_line):
 	comment_line = comment_line.strip()
@@ -21,93 +21,58 @@ def check_empty(comment_line):
 	return comment_line.strip() in ['', '*']
 
 def check_end_block_comment(comment_line):
-	return (comment_line.find("'''") != -1 or
-			comment_line.find('"""') != -1 or
-			comment_line.find('*/') != -1 or
-			comment_line.find('-->') != -1 or
-			comment_line.find('}}') != -1 or
+	not_end_block = (comment_line.find("'''") == -1 and
+			comment_line.find('"""') != -1 and
+			comment_line.find('*/') != -1 and
+			comment_line.find('-->') != -1 and
+			comment_line.find('}}') != -1 and
 			comment_line.find('-#}') != -1)
+	return not not_endblock #dumb way to write it before
 
-def check_trailing_comment(comment_line):
+def check_trailing_comment(comment_line): 
 	m = re.search('\S+\s*#\s*TODO.+$', comment_line)
 	return m is not None
 
 def expand(match):
-	all_comment_lines = [match.comment_line]
-	if check_trailing_comment(match.comment_line):
+	all_comment_lines = [match.comment_line] #make sense?
+	if check_trailing_comment(match.comment_line): #doesn't do trailing comments. Big error.
 		pass
-	elif check_comment_line(match.comment_line):
+	elif check_comment_line(match.comment_line): #if this is a comment, check if the next line is.
 		start = match.comment_line_number + 1
 		comment_line = comment_linecache.getcomment_line(match.file, start)
-		while check_comment_line(comment_line) and not check_todo(comment_line):
+		while check_comment_line(comment_line) and not check_todo(comment_line): #select all lines that are still comments, and append those to all.
 			all_comment_lines.append(comment_linecache.getcomment_line(match.file, start))
 			start += 1
 			comment_line = comment_linecache.getcomment_line(match.file, start)
-	elif not check_end_block_comment(match.comment_line):
-		start = match.comment_line_number + 1
+	elif not check_end_block_comment(match.comment_line): #ah damn, the current match is not the end of a block comment
+		start = match.comment_line_number + 1 #what about the next one?
 		comment_line = comment_linecache.getcomment_line(match.file, start)
-		while (not check_empty(comment_line) and
-			   not check_todo(comment_line) and
-			   not check_end_block_comment(comment_line)):
-			all_comment_lines.append(comment_linecache.getcomment_line(match.file, start))
+		while (not check_empty(comment_line) and not check_todo(comment_line) and not check_end_block_comment(comment_line)):
+			all_comment_lines.append(comment_linecache.getcomment_line(match.file, start)) #lets append that bitch.
 			start += 1
 			comment_line = comment_linecache.getcomment_line(match.file, start)
 
 	return all_comment_lines
 		
-def extract_names(comment_line):
+def extract_titles(comment_line):
 	contents = re.findall('TODO\((.*?)\)', comment_line)
 
-	# names must be alphanumeric or _-. Commas and slash OK as seperators.
 	if not all([re.match('^[a-zA-Z0-9_\-,/ ]+$', c) for c in contents]):
 		return None
 
-	names = []
-	for name in contents:
-		names.extend([x.strip() for x in re.split('[,/]', name)])
+	titles = []
+	for title in contents:
+		titles.extend([x.strip() for x in re.split('[,/]', title)])
 
-	return names
+	return titles #TODO(nbellowe) sucks, do this yourself Nathan.
 
 def get_todo_matches():
 	try:
-		matches = subprocess.check_output(['ack', '--with-filename', "TODO\\(.*\\)"])
-	except subprocess.CalledProcessError, e:
+		matches = subprocess.check_output(['ack', '--with-filename', "TODO\\(.*\\)"]) #http://beyondgrep.com/
+	except subprocess.CalledProcessError, e: #TODO TEST
 		print e.output
 		raise e
 	matches = [l for l in matches.split('\n') if l.strip() != '']
 	matches = [l.split(':', 2) for l in matches]
-	matches = [Match(l[0], int(l[1]), l[2]) for l in matches]
-
+	matches = [Flag_Line(l[0], int(l[1]), l[2]) for l in matches] #high performance data type. Feel free to get rid of, just think of it as a special communication message. All this does is takes the list of matches and create a new list of "Flag_Line" type.
 	return matches
-
-def interactive(match_names=None, show_count=False):
-	matches = get_todo_matches()
-
-	count = 0
-	count_by_name = Counter()
-
-	for match in matches:
-		result_comment_lines = expand(match)
-		names = extract_names(match.comment_line)
-
-		if names == None:
-			continue
-
-		if match_names != []:
-			if not any([n in names for n in match_names]):
-				continue
-
-		count += 1
-		for n in names:
-			count_by_name[n] += 1
-
-		if not show_count:
-			print '%s:%d' % (match.file, match.comment_line_number), ', '.join(names)
-			for l in result_comment_lines:
-				print l.strip()
-			print
-	if show_count and match_names != []:
-		print '%d TODOs' % count
-	elif show_count and match_names == []:
-		for name, count in count_by_name.most_common():
-			print name, count
