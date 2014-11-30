d4c3021a57048d6cecd2ad42dde2357d12800c89
Jason Kinnard <kinnard.jason@gmail.com>
2014-11-29 21:55:46 -0700

    changed to implement ff_db class, waaay cleaner

--- ff/database.py
+++ ff/database.py
@@ -1,54 +1,49 @@
 import os
 import sqlite3
-import namedtuple
+from collections import namedtuple
 
-#project_rootdir = os.environ['PWD']
+DB_DIRNAME = '.ff'
+DB_ENTRY_ITEMS = 3
+DB_ENTRY_CATEGORIES = '(file text, line_number integer, comment_line text)'
 
-def db_exists(dirname, filename):
+class ff_db:
+	
+	def __init__(self, project_rootdir, db_name):
 
-    abs_filepath = os.path.join(dirname, filename)	
+		self.abs_dirpath = os.path.join(project_rootdir, DB_DIRNAME)
+		if os.path.isdir(self.abs_dirpath) == False:
+			try:
+				os.mkdir(self.abs_dirpath)
+			except OSError:
+				raise
 
-    if os.path.isdir(dirname):
-        if os.path.isfile(abs_filepath + '-ff.db'): 
-            return True
-    return False
+		self.abs_filepath = os.path.join(self.abs_dirpath, db_name)
+		self.db_conn = sqlite3.connect(self.abs_filepath)
+		self.db_cursor = self.db_conn.cursor()
+		db_entry_string = 'CREATE TABLE IF NOT EXISTS flags ' + DB_ENTRY_CATEGORIES
+		self.db_cursor.execute(db_entry_string)
+		self.db_conn.commit()
 
-def db_add_entry(project_rootdir, db_entry):
+	def close(self):
+		self.db_cursor.close()
+		self.db_conn.close()
+	
+	def add_entry(self, db_entry):
+		db_entry_string = '('
+		for i in db_entry:
+			db_entry_string += '?,' 
+		db_entry_string = db_entry_string[:len(db_entry_string) - 1] + ')'
+		db_entry_string = 'INSERT INTO flags VALUES ' + db_entry_string
+		self.db_cursor.execute(db_entry_string, db_entry[:DB_ENTRY_ITEMS])
+		self.db_conn.commit
+		
+	def retrieve(self, query_type, *db_search_query):
+		query_results = []
+		if query_type == 'file':	
+			db_search_query = (db_search_query,)
+			self.db_cursor.execute('SELECT * FROM flags WHERE file LIKE ?', db_search_query)
+			for row in self.db_cursor:
+				query_results.append(row)
+			print query_results
+		
 
-	#db_entry = Some amorphous blob, to grow and shrink as per the whims of parse.py. Accepted into this function as a namedtuple.
-
-	#project_rootdir = This is the root directory of the project. Orignally hardcoded in as os.environ['PWD'], which gives current working directory. Nathan wisely talked me into making it relative.
-
-    def make_dbapi_param_string(db_entry):
-        entry_string = ''
-        if db_entry == ():
-            print 'Error: empty entry supplied as argument' #TODO: return a better error than this
-        for i in range(0, len(db_entry)):
-            entry_string += '?,'
-        return entry_string[:(len(entry_string)-3)]  #quick and dirty way of shaving off an entry so the 'file' entry can be hardcoded in somewhere outside this string. The file entry is extra important -- it's how we organize our db.
-
-    #def setup_db_input_args(db_entry):
-    #    input_arg_string = ''
-    #    for i in range(0, len(db_entry)):
-    #        input_arg_string = input_arg_string + db_entry[i]
-    #    return input_arg_string[:len(input_arg_string)-2] #take off trailing comma and whitespace, sanatize our input
-
-    db_dirname = os.path.join(project_rootdir, '.ff')
-    db_filename = os.path.split(project_rootdir)[1] + '-ff.db'
-    db_column_names = 'comment_line_number, comment_line' #remember, if this ever changes make sure db_entry element count matches up.
-
-    if db_exists(db_dirname, db_filename) == False:
-        try:
-            os.mkdir(db_dirname)
-        except OSError:
-            return 1
-
-    db_conn = sqlite3.connect(os.path.join(db_dirname, db_filename))
-    db_cursor = db_conn.cursor()
-
-    dbapi_param_string = make_dbapi_param_string(db_entry)
-    db_cursor.execute('CREATE TABLE IF NOT EXISTS ? (' + dbapi_param_string + ')', db_column_names)
-    #does entry exist?
-    #to check, I'll have to implement retrieve(db_entry), which will be eventually wrapped by db_retrieve(search_query)
-
-    db_cursor.execute('INSERT INTO ? VALUES (' + dbapi_param_string + ')', db_entry) 
