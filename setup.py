2e3c18b111c9cb274c4b9781be718764e6e7c5ce
Alex Tzinov <alex.tzinov@gmail.com>
2014-12-04 23:52:39 -0700

    Added entry point so that listTags can be called

--- setup.py
+++ setup.py
@@ -53,6 +53,7 @@ setup(
         'd = ff.display_tag:DispTag',
         'parse = ff.flag_finder_main:Main',
         'setup = ff.flag_finder_main:Setup',
+        'list = ff.listTags:Files',
         ],
     },
 
